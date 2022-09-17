ad26d7699bfd72929914598c5d5767cfbb16d8f6
maho1031 <mahohattori0428@gmail.com>
2022-09-17 23:10:40 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+// import GuestLayout from 'vendor/laravel/breeze/stubs/inertia-vue/resources/js/Layouts/GuestLayout.vue';
+
+const emitTest = e => console.log(e)
+
+</script>
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input 
+        modelValue="初期値"
+        @update:modelValue="emitTest"
+        ></Input>
+    </GuestLayout>
+    
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\InertiaTestController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+});
+Route::get('/component-test', function () {
+    return Inertia::render('ComponentTest');
+});
+
+Route::get('/inertia/index', [inertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('/inertia/show/{id}', [inertiaTestController::class, 'show'])->name('inertia.show');
+Route::get('/inertia/create', [inertiaTestController::class, 'create'])->name('inertia.create');
+Route::post('/inertia', [inertiaTestController::class, 'store'])->name('inertia.store');
+Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('inertia.delete');
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
